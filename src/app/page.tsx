52f99db5e4a8b0753fb318530794c462d47a1af7
Ewerton Freitas <ewerton12freitas@gmail.com>
2023-05-30 18:18:07 -0300

    swiper test 01

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,24 +1,42 @@
 "use client";
 
+import { useEffect, useState } from "react";
+
 // Import Swiper React components
 import { Swiper, SwiperSlide } from "swiper/react";
 
 // Import Swiper styles
 import "swiper/css";
 
+type Repository = {
+  full_name: string;
+  description: string;
+};
+
 export default function Home() {
+  const [repositories, setRepositories] = useState<Repository[]>([]);
+
+  useEffect(() => {
+    fetch("https://api.github.com/users/diego3g/repos")
+      .then((response) => response.json())
+      .then((data) => setRepositories(data));
+  }, []);
+
   return (
     <Swiper
       spaceBetween={50}
-      slidesPerView={3}
+      slidesPerView={4}
       onSlideChange={() => console.log("slide change")}
       onSwiper={(swiper) => console.log(swiper)}
     >
-      <SwiperSlide>Slide 1</SwiperSlide>
-      <SwiperSlide>Slide 2</SwiperSlide>
-      <SwiperSlide>Slide 3</SwiperSlide>
-      <SwiperSlide>Slide 4</SwiperSlide>
-      ...
+      {repositories.map(repo => {
+        return (
+          <SwiperSlide key={repo.full_name}>
+            <h1>{repo.full_name}</h1>
+            <p>{repo.description}</p>
+          </SwiperSlide>
+        );
+      })}
     </Swiper>
   );
 }
